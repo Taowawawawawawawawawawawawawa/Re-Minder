bd31be87382403d5902d0739ce7b6d64c06361ff
PiengpienNiyata <151113312+PiengpienNiyata@users.noreply.github.com>
2024-11-13 21:46:33 +0700

    push 2

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Navber from "./Compounents/Navber/Navbar";
+import Footer from './Compounents/Footer/footer';
+
+export default function App(){
+  return <><Navber />
+<div className="app-container">
+      {/* Main Quest Section */}
+      <div className="section quest-section">
+        <h1>เควสต์</h1>
+        <p>Quest</p>
+        <p className="refresh-timer">รีเฟรชในอีก 5.55.55 ชั่วโมง</p>
+      </div>
+
+      {/* Shop and Room Section */}
+      <div className="shop-room-container">
+        <div className="section shop-section">
+          <h2>ร้านค้า</h2>
+          <p>Shop</p>
+        </div>
+
+        <div className="section room-section">
+          <h2>ห้องพักใจ</h2>
+          <p>My room</p>
+        </div>
+      </div>
+    </div>
+  <Footer /></>
+}
