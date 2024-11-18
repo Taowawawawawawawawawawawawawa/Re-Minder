4e2b5719e28c0f69527c35934c3a571555f48138
Pattarapon <151112816+pond2tpkeiei@users.noreply.github.com>
2024-11-18 21:24:12 +0700

    test

--- SpringBoot/microservice-users/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Users (userId, name, dateOfBirth, email, password, mbti, beryl, points) VALUES
-(10001, 'John Doe', '1990-05-15', 'johndoe@example.com', 'securePassword123', 'INTJ', 100, 50),
-(10002, 'Jane Smith', '1988-07-20', 'janesmith@example.com', 'securePassword456', 'ENTP', 200, 75),
-(10003, 'Alice Johnson', '1995-03-12', 'alicej@example.com', 'securePassword789', 'INFJ', 50, 30),
-(10004, 'Bob Brown', '1992-11-25', 'bobbrown@example.com', 'securePassword321', 'ESTJ', 150, 20),
-(10005, 'Charlie Black', '1993-01-10', 'charlieb@example.com', 'securePassword654', 'ISFP', 300, 100);
